9fc929b15c787b87e4a76ddbaaa30c55c36c2ef6
Caian Marcaccini <87946826+CaianMarcaccini@users.noreply.github.com>
2022-05-11 23:01:49 -0300

    Add files via upload

---
+++ minesweeper/mine.py
@@ -0,0 +1,76 @@
+from tkinter import *
+import settings
+import utils
+from cell import Cell
+
+root = Tk()
+# sobrepoe config da tela 
+root.configure(bg="black")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title('Campo Minado')
+root.resizable(False, False)
+
+
+top_frame = Frame(
+    root,
+    bg = 'black',
+    width = settings.WIDTH,
+    height = utils.height_prct(25)
+)
+
+top_frame.place(x=0, y=0)
+
+game_tittle = Label(
+    top_frame,
+    bg= 'black',
+    fg='white',
+    text='Campo Minado',
+    font=('Verdana',38)
+)
+
+game_tittle.place(
+    x=utils.width_prct(25), 
+    y=0
+)
+
+left_frame = Frame (
+    root,
+    bg = 'black',
+    width= utils.width_prct(25),
+    height= utils.height_prct(75)
+)
+
+left_frame.place(x=0, y= utils.height_prct(25))
+
+
+center_frame = Frame(
+    root,
+    bg = 'black' ,
+    width = utils.width_prct(75),
+    height = utils.height_prct(75)
+)
+center_frame.place(x= utils.width_prct(25), y= utils.height_prct(25))
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x , y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+
+
+# chamar o label da classe cell
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(
+    x=0, y=0
+    )
+Cell.randomize_mines()
+
+
+
+
+#rodar a tela
+root.mainloop()
+
+
